47efd64e3919f0efea7db7bfed72e7a8a59022bb
jacobrs <jacob_gagne@hotmail.com>
2017-07-04 22:51:58 -0400

    implemented weighted A/B

--- mvp-router/src/components/SuggestPage/SuggestConfig.js
+++ mvp-router/src/components/SuggestPage/SuggestConfig.js
@@ -4,39 +4,87 @@ import PermContactCalendar from 'material-ui-icons/PermContactCalendar';
 import TagFaces from 'material-ui-icons/TagFaces';
 
 var config = {
-	id: "SUG-1",
-	hook: {
-		title: "Rediscover your free time",
-		text: "Use BreakMates to find new things to do with your friends",
-		button: "Be the first to do so"
+	weights: [
+		{
+			weight: 33,
+			name: "suggest"
+		},
+		{
+			weight: 100,
+			name: "alternate"
+		}
+	],
+	suggest: {
+		id: "SUG-1",
+		hook: {
+			title: "Rediscover your free time",
+			text: "Use BreakMates to find new things to do with your friends",
+			button: "Be the first to do so"
+		},
+		signuphook: {
+			title: "Be the first to Rediscover your free time",
+		},
+		register: {
+			title: "Register to our mailing list",
+			subtitle: "We'll notify you when we release our first beta",
+			hint: "Email Address",
+			button: "Signup for the beta now"
+		},
+		selling: {
+			points: [
+				{
+					icon: <FaceIcon/>,
+					hook: "Face to face",
+					text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+				},
+				{
+					icon: <TagFaces/>,
+					hook: "Easy to use",
+					text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+				},
+				{
+					icon: <PermContactCalendar/>,
+					hook: "Synced Calendar",
+					text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+				}
+			]
+		}
 	},
-	signuphook: {
-		title: "Be the first to Rediscover your free time",
-	},
-	register: {
-		title: "Register to our mailing list",
-		subtitle: "We'll notify you when we release our first beta",
-		hint: "Email Address",
-		button: "Signup for the beta now"
-	},
-	selling: {
-		points: [
-			{
-				icon: <FaceIcon/>,
-				hook: "Face to face",
-				text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
-			},
-			{
-				icon: <TagFaces/>,
-				hook: "Easy to use",
-				text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
-			},
-			{
-				icon: <PermContactCalendar/>,
-				hook: "Synced Calendar",
-				text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
-			}
-		]
+	alternate: {
+		id: "SUG-2",
+		hook: {
+			title: "Rediscover your free time 2",
+			text: "Use BreakMates to find new things to do with your friends",
+			button: "Be the first to do so"
+		},
+		signuphook: {
+			title: "Be the first to Rediscover your free time",
+		},
+		register: {
+			title: "Register to our mailing list",
+			subtitle: "We'll notify you when we release our first beta",
+			hint: "Email Address",
+			button: "Signup for the beta now"
+		},
+		selling: {
+			points: [
+				{
+					icon: <FaceIcon/>,
+					hook: "Face to face",
+					text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+				},
+				{
+					icon: <TagFaces/>,
+					hook: "Easy to use",
+					text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+				},
+				{
+					icon: <PermContactCalendar/>,
+					hook: "Synced Calendar",
+					text: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+				}
+			]
+		}
 	}
 }
 
--- mvp-router/src/components/SuggestPage/SuggestPage.js
+++ mvp-router/src/components/SuggestPage/SuggestPage.js
@@ -4,10 +4,20 @@ import config from './SuggestConfig';
 
 class SuggestPage extends Component {
 	render() {
-		window.mixpanel.track("Render-" + config.id);
+		var seed = Math.random() * 100;
+
+		var conf = null;
+		for(var i = 0; i < config.weights.length; i++){
+			if(config.weights[i].weight >= seed){
+				conf = config[config.weights[i].name];
+				break;
+			}
+		}
+
+		window.mixpanel.track("Render-" + conf.id);
 		return (
 				<div>
-					<LandingPage config={config}/>
+					<LandingPage config={conf}/>
 				</div>
 			)
 	}
