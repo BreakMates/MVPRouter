f0f74da857678d0b90377dcda82ceedcc96d3b47
jacobrs <jacob_gagne@hotmail.com>
2017-07-18 21:36:03 -0400

    fixed the mock

--- mvp-router/src/components/App/App.js
+++ mvp-router/src/components/App/App.js
@@ -4,25 +4,21 @@ import './App.css';
 class App extends Component {
 
 	componentWillMount() {
-		var MixpanelMock = (function() {
-      function MixpanelMock() {
-        this.init = function() {
+		var mixpanelMock = {
+        init: function() {
           console.log("mixpanel.init", arguments);
-        }
-        this.track = function() {
+        },
+        track: function() {
           console.log("mixpanel.track", arguments);
         }
-      }
-
-      return MixpanelMock;
-
-    })();
+      };
 
     if(window.location.host.split(":")[0] !== "localhost" && window.location.protocol !== "https:"){
       window.location = "https://lander.breakmates.com";
     }
     if(window.location.host.split(":")[0] !== "lander.breakmates.com"){
-      window.mixpanel = new MixpanelMock();
+    	window.mixpanel.init = mixpanelMock.init;
+    	window.mixpanel.track = mixpanelMock.track;
     }
   	window.mixpanel.track("Hit-Load-Balancer");
 	}
--- mvp-router/src/components/App/App.test.js
+++ mvp-router/src/components/App/App.test.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
+
+  window.mixpanel = {
+      init: function() {
+        console.log("mixpanel.init", arguments);
+      },
+      track: function() {
+        console.log("mixpanel.track", arguments);
+      }
+    };
+    
   const div = document.createElement('div');
   ReactDOM.render(<App/>, div);
 });
