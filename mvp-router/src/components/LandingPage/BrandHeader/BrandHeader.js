922173f421e9b808b0372a483e9ed56324e3983f
jacobrs <jacob_gagne@hotmail.com>
2017-07-18 20:42:58 -0400

    now cycles between 2 different pictures

--- mvp-router/src/components/LandingPage/BrandHeader/BrandHeader.js
+++ mvp-router/src/components/LandingPage/BrandHeader/BrandHeader.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './BrandHeader.css';
 import Button from 'material-ui/Button';
 import Mockup from './../../../resources/mockup.png';
+import Screenshot1 from './../../../resources/sug-1.png';
+import Screenshot2 from './../../../resources/sug-2.png';
 
 class BrandHeader extends Component {
 
@@ -10,10 +12,25 @@ class BrandHeader extends Component {
    	document.getElementById("email").focus();
 	}
 
+	changeImage() {
+		var url = document.getElementById('mockup').src;
+		if (url === window.url1) {
+      document.getElementById('mockup').src = Screenshot1;
+    } else {
+      document.getElementById('mockup').src = Screenshot2;
+    }
+	}
+
+	componentDidMount() {
+    window.url1 = document.getElementById('mockup').src;
+		window.setInterval(this.changeImage, 5000);
+	}
+
 	render(){
 		return (
 				<div className="container">
 					<img alt="phone frame" src={Mockup} className="phone-mock"/><br/>
+					<img alt="phone mockup" src={Screenshot2} className="screen-mock" id="mockup"/><br/>
 					<h2>{this.props.hook.title}</h2><br/>
 					<p className="hookText">{this.props.hook.text}</p>
 					<Button raised color="accent" onClick={this.scrollIntoView}>{this.props.hook.button}</Button>
