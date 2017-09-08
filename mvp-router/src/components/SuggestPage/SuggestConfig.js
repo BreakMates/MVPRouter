8572794e9c2d98b6ad022c8b8d394b4ca0f4e3dc
jonnyrs <jonnyrs@users.noreply.github.com>
2017-09-07 21:21:08 -0400

    Update SuggestConfig.js
    Added A/B tests, 3 versions, testing for the best title 1. GG

--- mvp-router/src/components/SuggestPage/SuggestConfig.js
+++ mvp-router/src/components/SuggestPage/SuggestConfig.js
@@ -1,8 +1,16 @@
 var config = {
 	weights: [
 		{
-			weight: 100,
+			weight: 33,
 			name: "suggest"
+		},
+		{
+			weight: 66,
+			name: "suggest2"
+		},
+		{
+			weight: 100,
+			name: "suggest3"
 		}
 	],
 	suggest: {
@@ -13,6 +21,86 @@ var config = {
 		],
 		hook: {
 			title: "Rediscover your free time",
+			text: "Use BreakMates to find new activities nearby.",
+			button: "Be the first to do so"
+		},
+		signuphook: {
+			title: "Be the first to rediscover your free time",
+		},
+		register: {
+			title: "Register to our mailing list",
+			subtitle: "We'll notify you when we release our first beta",
+			hint: "Email Address",
+			button: "Signup for the beta now"
+		},
+		selling: {
+			points: [
+				{
+					headline: "Activity Suggestions",
+					details: "BreakMates makes planning easier by helping you decide what to do, with curated activity suggestions based on your interests.",
+					imageUrl: "/images/features-1.png"
+				},
+				{
+					headline: "Suggested Locations",
+					details: "Each activity includes a list of places for you to visit with your friends.",
+					imageUrl: "/images/features-2-V2.png"
+				},
+				{
+					headline: "Group Chat",
+					details: "Make the plan, keep all the info in one place and chat with your friends.",
+					imageUrl: "/images/features-3.png"
+				}
+			]
+		}
+	},
+	suggest2: {
+		id: "SUG-2",
+		mocks: [
+			"/images/sug-1.png",
+			"/images/sug-2.png"
+		],
+		hook: {
+			title: "Discover fun things to do nearby",
+			text: "Use BreakMates to find new activities nearby.",
+			button: "Be the first to do so"
+		},
+		signuphook: {
+			title: "Be the first to rediscover your free time",
+		},
+		register: {
+			title: "Register to our mailing list",
+			subtitle: "We'll notify you when we release our first beta",
+			hint: "Email Address",
+			button: "Signup for the beta now"
+		},
+		selling: {
+			points: [
+				{
+					headline: "Activity Suggestions",
+					details: "BreakMates makes planning easier by helping you decide what to do, with curated activity suggestions based on your interests.",
+					imageUrl: "/images/features-1.png"
+				},
+				{
+					headline: "Suggested Locations",
+					details: "Each activity includes a list of places for you to visit with your friends.",
+					imageUrl: "/images/features-2-V2.png"
+				},
+				{
+					headline: "Group Chat",
+					details: "Make the plan, keep all the info in one place and chat with your friends.",
+					imageUrl: "/images/features-3.png"
+				}
+			]
+		}
+	},
+	suggest3: {
+		id: "SUG-3",
+		mocks: [
+			"/images/sug-1.png",
+			"/images/sug-2.png"
+		],
+		hook: {
+			title: "Awesome activities, just for you",
 			text: "Use BreakMates to find new activities nearby.",
 			button: "Be the first to do so"
 		},
